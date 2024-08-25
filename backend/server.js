8267665c4f5857d16aec38751c124b76f294759c
Brian Michael <brianmichaeladero@gmail.com>
2024-08-25 12:31:55 +0300

    backend

--- backend/server.js
+++ backend/server.js
@@ -101,13 +101,19 @@ let PORT = process.env.PORT || 3000;
 //   });
 // }
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+app.use(express.static(path.join(__dirname, "frontend/dist")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../frontend/dist/index.html"));
-  });
-}
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+});
+
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "../frontend/dist/index.html"));
+//   });
+// }
 
 app.listen(PORT, () =>
   console.log(`server started at http://localhost:${PORT}`)
