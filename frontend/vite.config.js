432fbd781fdbbd0ba7982ba87e0c71ef444b0f9e
Brian Michael <brianmichaeladero@gmail.com>
2024-08-26 06:46:03 +0300

    update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,17 +1,38 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react-swc";
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     // port: 5173,
+//     proxy: {
+//       "/api": {
+//         target: "http://localhost:3000",
+//         changeOrigin: true,
+//         secure: false,
+//       },
+//     },
+//   },
+// });
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    // port: 5173,
-    proxy: {
-      "/api": {
-        target: "http://localhost:3000",
-        changeOrigin: true,
-        secure: false,
-      },
+export default defineConfig(({ mode }) => {
+  const isProduction = mode === "production";
+
+  return {
+    plugins: [react()],
+    server: {
+      proxy: isProduction
+        ? {}
+        : {
+            "/api": {
+              target: "http://localhost:3000",
+              changeOrigin: true,
+              secure: false,
+            },
+          },
     },
-  },
+  };
 });
