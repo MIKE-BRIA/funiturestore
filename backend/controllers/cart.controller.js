4c9c21b533de52bedadd37b42b18ab93a2c5e457
Brian Michael <brianmichaeladero@gmail.com>
2024-09-14 18:10:22 +0300

    updated

--- backend/controllers/cart.controller.js
+++ backend/controllers/cart.controller.js
@@ -1,3 +1,206 @@
+// import Cart from "../models/cartModel.js";
+
+// export async function addToCart(req, res) {
+//   try {
+//     const { name, img, price, quantity, user, productId } = req.body;
+
+//     if (!name || !img || !quantity || !price)
+//       return res
+//         .status(400)
+//         .json({ error: "all product required fields must be provided" });
+
+//     let cartItem = await Cart.findOne({ productId, user });
+
+//     if (cartItem) {
+//       cartItem.quantity += quantity;
+//       cartItem.totalPrice = cartItem.price * cartItem.quantity;
+//     } else {
+//       const totalPrice = price * quantity;
+
+//       cartItem = await Cart.create({
+//         name,
+//         img,
+//         price,
+//         quantity,
+//         user,
+//         productId,
+//         totalPrice,
+//       });
+//     }
+
+//     await cartItem.save();
+
+//     return res
+//       .status(200)
+//       .json({ message: "cart added successfully", cartItem: cartItem });
+//   } catch (error) {
+//     console.log("error in addtocart: ", error.message);
+//     return res.status(500).json({ error: error.message });
+//   }
+// }
+
+// export async function getCart(req, res) {
+//   try {
+//     const { userId } = req.params;
+//     const cart = await Cart.find({ user: userId });
+//     res.status(200).json(cart);
+//   } catch (error) {
+//     console.log("error in getCart: ", error.message);
+//     return res.status(500).json({ error: error.message });
+//   }
+// }
+
+// export async function updatecartdata(req, res) {
+//   try {
+//     const { cartItemId, quantity } = req.body;
+
+//     if (!cartItemId || !quantity)
+//       return res.status(404).json({ error: "cartItem Id is required" });
+
+//     const cartItem = await Cart.findById(cartItemId);
+
+//     if (!cartItem)
+//       return res.status(404).json({ error: "Cart item not found" });
+
+//     cartItem.quantity = quantity;
+//     cartItem.totalPrice = cartItem.price * quantity;
+
+//     await cartItem.save();
+
+//     return res
+//       .status(200)
+//       .json({ message: "Cart updated successfully", cartItem });
+//   } catch (error) {
+//     console.log("error in updatecartdata: ", error.message);
+//     return res.status(500).json({ error: error.message });
+//   }
+// }
+
+// export async function removeItemFromCart(req, res) {
+//   try {
+//     const { cartId } = req.params;
+
+//     if (!cartId)
+//       return res.status(404).json({ error: "CartID does not exist" });
+
+//     const CartItem = await Cart.findById(cartId);
+
+//     if (!CartItem) return res.status(404).json({ error: "Item not found" });
+
+//     await Cart.findByIdAndDelete(cartId);
+
+//     res.status(200).json({ message: "Cart item deleted successfully" });
+//   } catch (error) {
+//     res.status(500).json({ error: error.message });
+//     console.log("error in removeItemFromCart: ", error.message);
+//   }
+// }
+
+// import Cart from "../models/cartModel.js";
+
+// export async function addToCart(req, res) {
+//   try {
+//     const { name, img, price, quantity, user, productId } = req.body;
+
+//     if (!name || !img || !quantity || !price)
+//       return res
+//         .status(400)
+//         .json({ error: "all product required fields must be provided" });
+
+//     const totalPrice = price * quantity;
+
+//     const cartItem = await Cart.create({
+//       name,
+//       img,
+//       price,
+//       quantity,
+//       user,
+//       productId,
+//       totalPrice,
+//     });
+
+//     return res
+//       .status(200)
+//       .json({ message: "cart added successfully", cartItem: cartItem });
+//   } catch (error) {
+//     console.log("error in addtocart: ", error.message);
+//     return res.status(500).json({ error: error.message });
+//   }
+// }
+
+// export async function getCart(req, res) {
+//   try {
+//     const { userId } = req.params;
+//     const cart = await Cart.find({ user: userId });
+//     res.status(200).json(cart);
+//   } catch (error) {
+//     console.log("error in getCart: ", error.message);
+//     return res.status(500).json({ error: error.message });
+//   }
+// }
+
+// export async function updatecartdata(req, res) {
+//   try {
+//     const { cartItemId, quantity } = req.body;
+
+//     if (!cartItemId || !quantity)
+//       return res
+//         .status(400)
+//         .json({ error: "cart item ID and Quantity are missing" });
+
+//     const cartItem = await Cart.findById(cartItemId);
+
+//     if (!cartItem)
+//       return res.status(400).json({ error: "cart item not found" });
+
+//     cartItem.quantity = quantity;
+//     cartItem.totalPrice = cartItem.price * quantity;
+
+//     await cartItem.save();
+
+//     return res
+//       .status(200)
+//       .json({ message: "Cart item updated successfully", cartItem });
+//   } catch (error) {
+//     console.log("error in updatecartdata: ", error.message);
+//     return res.status(500).json({ error: error.message });
+//   }
+// }
+
+// export async function removeCartProduct(req, res) {
+//   try {
+//     const { cartId } = req.params;
+
+//     const product = await Cart.findById(cartId);
+
+//     if (!product) return res.status(404).json({ error: "product not found" });
+
+//     await Cart.findByIdAndDelete(cartId);
+
+//     res.status(200).json({ message: "Cart product deleted successfully" });
+//   } catch (error) {
+//     console.log("error in removeCartProduct: ", error.message);
+//     return res.status(500).json({ error: error.message });
+//   }
+// }
+
+// export async function getcartProduct(req, res) {
+//   try {
+//     const { productId } = req.params;
+
+//     const product = await Cart.findById(productId);
+
+//     if (!product) {
+//       return res.status(404).json({ error: "Product not found" });
+//     }
+
+//     res.status(200).json({ message: "Product found", product });
+//   } catch (error) {
+//     console.log("Error in getcartProduct: ", error.message);
+//     return res.status(500).json({ error: error.message });
+//   }
+// }
+
 import Cart from "../models/cartModel.js";
 
 export async function addToCart(req, res) {
@@ -55,12 +258,14 @@ export async function updatecartdata(req, res) {
     const { cartItemId, quantity } = req.body;
 
     if (!cartItemId || !quantity)
-      return res.status(404).json({ error: "cartItem Id is required" });
+      return res
+        .status(400)
+        .json({ error: "cart item ID and Quantity are missing" });
 
     const cartItem = await Cart.findById(cartItemId);
 
     if (!cartItem)
-      return res.status(404).json({ error: "Cart item not found" });
+      return res.status(400).json({ error: "cart item not found" });
 
     cartItem.quantity = quantity;
     cartItem.totalPrice = cartItem.price * quantity;
@@ -69,26 +274,9 @@ export async function updatecartdata(req, res) {
 
     return res
       .status(200)
-      .json({ message: "Cart updated successfully", cartItem });
+      .json({ message: "Cart item updated successfully", cartItem });
   } catch (error) {
     console.log("error in updatecartdata: ", error.message);
-    return res.status(500).json({ error: error.message });
-  }
-}
-
-export async function removeCartProduct(req, res) {
-  try {
-    const { cartId } = req.params;
-
-    const product = await Cart.findById(cartId);
-
-    if (!product) return res.status(404).json({ error: "product not found" });
-
-    await Cart.findByIdAndDelete(cartId);
-
-    res.status(200).json({ message: "Cart product deleted successfully" });
-  } catch (error) {
-    console.log("error in removeCartProduct: ", error.message);
     return res.status(500).json({ error: error.message });
   }
 }
