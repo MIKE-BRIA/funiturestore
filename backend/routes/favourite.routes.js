86bbaee8cfe609eff564c55e8e7bd86515f6030b
Brian Michael <brianmichaeladero@gmail.com>
2024-09-17 05:03:49 +0300

    routes

--- backend/routes/favourite.routes.js
+++ backend/routes/favourite.routes.js
@@ -3,6 +3,7 @@ import {
   addToFavourite,
   deleteFavourites,
   getAllFavourites,
+  removeItem,
 } from "../controllers/fav.controller.js";
 
 const router = express.Router();
@@ -10,5 +11,6 @@ const router = express.Router();
 router.post("/addtofavourite", addToFavourite);
 router.get("/getFavourites/:userId", getAllFavourites);
 router.delete("/deleteFavourite/:userId", deleteFavourites);
+router.delete("/removeitem/:userId/:productId", removeItem);
 
 export default router;
